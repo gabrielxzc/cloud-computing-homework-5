322194c79e27eb828ea221b7d065574f3bdf6377
alex.mititelu <mititelu.alex@yahoo.com>
2019-05-10 01:07:59 +0300

    Reviews Preview

--- application.py
+++ application.py
@@ -5,6 +5,9 @@ import os
 import json
 from azure.storage.blob import BlockBlobService
 import uuid
+import ssl
+import json
+
 
 app = Flask(__name__)
 
@@ -35,6 +38,7 @@ def post_review():
         collection = db['reviews']
 
         collection.insert_one(data)
+
     elif 'audio' in data:
         block_blob_service = BlockBlobService(
             account_name='cch5blobstorage', account_key=os.getenv('BLOB_STORAGE_KEY'))
@@ -45,3 +49,44 @@ def post_review():
         return '', 400
 
     return '', 201
+
+
+def get_audio_blob(blob_name):
+    try:
+        my_account_key = "Pym9tZBPZgtfy4m84YbYQ37HJNJq4SfmvpJW8DyDkvWcTjfkwNABoV7nWoSZlcRRQSD7sMiJQMf3003nmQjtKA=="
+        # os.getenv('BLOB_STORAGE_KEY')
+        block_blob_service = BlockBlobService(
+            account_name='cch5blobstorage', account_key=my_account_key)
+        container_name = 'review-audio-blobs'
+        return block_blob_service.get_blob_to_text(container_name, blob_name).content
+    except:
+        return ""
+
+
+@app.route('/api/reviews', methods=['GET'])
+def get_reviews_content():
+
+    # client = pymongo.MongoClient(os.getenv("CUSTOMCONNSTR_MONGOURL"))
+    URL = "mongodb://cloud-computing-homework-5-db:ytQRJXRuos4Bu7zWAvbraicCh0CVjwoekMr4MBXw2Ad35ZLOjlhfTyRoP2Hsu1bUYFaKCWLijUYv4hLoRRpAeA==@cloud-computing-homework-5-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+    client = pymongo.MongoClient(URL, ssl_cert_reqs=ssl.CERT_NONE)
+
+    db = client['cloud-computing-homework-5-db']
+    collection = db['reviews']
+    cursor = collection.find()
+
+    results = []
+    for elem in cursor:
+        if "analysis" not in elem.keys():
+            continue
+        obj = {}
+        obj["text"] = elem["text"]
+
+        if "analysis" in elem.keys():
+            obj["analysis"] = elem["analysis"]
+
+        if "file-name" in elem.keys():
+            obj["audio"] = get_audio_blob(
+                "ecd97de8-7284-11e9-b833-0242ac100102")
+        results.append(json.dumps(obj))
+
+    return json.dumps(results)
--- static/assets/js/reviews.js
+++ static/assets/js/reviews.js
@@ -1,1 +1,124 @@
-console.log('reviews.js');
+console.log("reviews.js");
+const audioData = [];
+
+let makeRequest = function(url, method) {
+  let request = new XMLHttpRequest();
+
+  return new Promise(function(resolve, reject) {
+    request.onreadystatechange = function() {
+      if (request.readyState !== 4) return;
+      if (request.status >= 200 && request.status < 300) {
+        resolve(JSON.parse(request.response));
+      } else {
+        reject({
+          status: request.status,
+          statusText: request.statusText
+        });
+      }
+    };
+
+    request.open(method || "GET", url, true);
+    request.send();
+  });
+};
+
+let displayReviews = function(reviews) {
+  if (reviews.length === 0) {
+    let message = document.createElement("span");
+    message.appendChild(document.createTextNode("No reviews found"));
+    return message;
+  } else {
+    let table = document.createElement("table");
+
+    let header = document.createElement("thead");
+    let headerRow = document.createElement("tr");
+
+    let textCol = document.createElement("td");
+    textCol.appendChild(document.createTextNode("Review"));
+
+    let analysis = document.createElement("td");
+    analysis.appendChild(document.createTextNode("Analysis"));
+
+    let audioSection = document.createElement("td");
+    audioSection.appendChild(document.createTextNode("Audio"));
+
+    headerRow.appendChild(textCol);
+    headerRow.appendChild(analysis);
+    headerRow.appendChild(audioSection);
+
+    header.appendChild(headerRow);
+
+    table.appendChild(header);
+
+    let tableBody = document.createElement("tbody");
+
+    for (let i = 0; i < reviews.length; i++) {
+      let element = JSON.parse(reviews[i]);
+      console.log(element);
+      let reviewRow = document.createElement("tr");
+
+      let reviewText = document.createElement("td");
+      reviewText.appendChild(document.createTextNode(element.text));
+
+      let reviewAnalysis = document.createElement("td");
+      reviewAnalysis.appendChild(
+        document.createTextNode(element.analysis ? element.analysis : "-")
+      );
+      console.log(element.analysis);
+
+      let reviewAudio = document.createElement("td");
+
+      if (element.audio) {
+        let reviewAudioPlayButton = document.createElement("audio");
+        reviewAudioPlayButton.setAttribute("src", element.audio);
+        reviewAudioPlayButton.setAttribute("controls", true);
+
+        reviewAudio.appendChild(reviewAudioPlayButton);
+      } else {
+        reviewAudio.appendChild(document.createTextNode("-"));
+      }
+
+      reviewRow.appendChild(reviewText);
+      reviewRow.appendChild(reviewAnalysis);
+      reviewRow.appendChild(reviewAudio);
+
+      tableBody.appendChild(reviewRow);
+    }
+
+    table.appendChild(tableBody);
+    return table;
+  }
+};
+
+let displayError = function() {
+  let errorMessage = document.createElement("span");
+  errorMessage.appendChild(document.createTextNode("Error"));
+
+  return errorMessage;
+};
+
+function initApplication() {
+  const modal = document.getElementById("reviews-module");
+
+  const title = document.createElement("h1");
+  title.appendChild(document.createTextNode("Reviews:"));
+
+  modal.appendChild(title);
+
+  makeRequest(window.location.origin + "/api/reviews")
+    .then(reviews => {
+      let reviewsTable = displayReviews(reviews);
+
+      modal.appendChild(reviewsTable);
+    })
+    .catch(err => {
+      console.log(err);
+      modal.appendChild(displayError());
+    });
+}
+
+document.onreadystatechange = function() {
+  if (document.readyState === "complete") {
+    initApplication();
+  }
+};
